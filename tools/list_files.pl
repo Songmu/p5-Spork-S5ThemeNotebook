800c7378ca30d4a10ee847f452a3a17d3f51070f
Masayuki Matsuki <y.songmu@gmail.com>
2012-09-24 01:07:11 +0900

    やっつけだけどツール作成

--- tools/list_files.pl
+++ tools/list_files.pl
@@ -2,13 +2,22 @@
 use 5.12.0;
 use warnings;
 use utf8;
+use lib 'lib';
 use Path::Class qw/file dir/;
 
-my $pm_file = file('lib/Spork/S5ThemeNotebook.pm');
-my $fh = $pm_file->open('a');
+use Spork::S5ThemeNotebook;
 
-my $path = 'src';
-my $dir = dir($path);
+my $pm_file = file('lib/Spork/S5ThemeNotebook.pm');
+my $content = $pm_file->slurp;
+
+$content =~ s/\n=cut\n.*/\n=cut\n\n/ms;
+
+my $fh = $pm_file->openw;
+
+$fh->print($content);
+
+my $asset_path = 'src';
+my $dir = dir($asset_path);
 
 $dir->recurse(
     callback => sub {
@@ -16,13 +25,14 @@ $dir->recurse(
         return unless -f $file;
 
         my $src_path = $file->relative;
-        $src_path =~ s!^$path/!!;
+        $src_path =~ s!^$asset_path/!!;
 
         $src_path = '__'.$src_path.'__';
 
         $fh->print($src_path."\n\n");
     },
 );
+$fh->close;
 
-
+`perl -Ilib -MSpork::S5ThemeNotebook -E 'Spork::S5ThemeNotebook->new->compress_files("src")'`
 
